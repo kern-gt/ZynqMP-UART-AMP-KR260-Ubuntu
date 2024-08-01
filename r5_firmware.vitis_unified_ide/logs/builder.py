6777cdcaf29a6a2c84a67dd3da261b03627b8814
sh-goto <sh.goto7000@gmail.com>
2024-08-02 04:34:28 +0900

    Replaced the Microblaze Vitis project with Export ZIP. Microblaze Vitis プロジェクトをExport ZIPに置き換えた。

--- r5_firmware.vitis_unified_ide/logs/builder.py
+++ r5_firmware.vitis_unified_ide/logs/builder.py
@@ -1,8 +1,11 @@
-# 2024-08-02T03:16:56.429375
+# 2024-08-02T03:57:07.904120
 import vitis
 
 client = vitis.create_client()
 client.set_workspace(path="/home/kerngt/work/zynqmp/xli_v2024_1_wk/kr260_amp_uart_release/software_wk/ZynqMP-UART-AMP-KR260-Ubuntu/r5_firmware.vitis_unified_ide")
+
+comp = client.get_component(name="app_echo_uart_r5_1")
+comp.build()
 
 platform = client.get_component(name="pf_amp_uart_r5_0")
 status = platform.build()
@@ -15,14 +18,12 @@ status = comp.clean()
 
 comp.build()
 
-comp = client.get_component(name="app_echo_uart_r5_1")
-status = comp.clean()
-
-comp.build()
-
-comp = client.get_component(name="app_echo_uart_r5_0")
 status = comp.clean()
 
 comp = client.get_component(name="app_echo_uart_r5_1")
 status = comp.clean()
+
+status = platform.build()
+
+vitis.dispose()
 
