253664f80ce0bf9cd380669c03b8b8e95c503f29
sh-goto <sh.goto7000@gmail.com>
2024-08-17 19:45:05 +0900

    Fixed PL DTOverlay generation process; added preprocessing to allow DTC compilation. PL DTOverlay生成処理を修正した。DTCコンパイルが可能になるように前処理を追加した。

--- hw_export/gen_pl_dts.sh
+++ hw_export/gen_pl_dts.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-#example $ bash ./gen_pl_dts.sh defalt_project_name
+#example
+#$ source /tools/Xilinx/Vitis/2024.1/settings64.sh
+#$ bash ./gen_pl_dts.sh hogehoge_project_name
 
 if [ -z "$1" ]; then
     PJ_NAME="defalt_project_name"
@@ -13,13 +15,13 @@ WORK_DIR=$(pwd)
 
 GEN_DTS_PATH="./my_dts/my_dts/${PJ_NAME}/psu_cortexa53_0/device_tree_domain/bsp/"
 PL_DTS_DIR="pl_dtso"
-TCL_CMD="createdts -hw ./design_1_wrapper.xsa -platform-name ${PJ_NAME} -git-branch xlnx_rel_v2024.1 -overlay -compile -out my_dts"
+TCL_CMD="createdts -hw ./design_1_wrapper.xsa -platform-name ${PJ_NAME} -overlay -out my_dts"
 
 
 echo ${TCL_CMD} > "createdts.tcl"
 xsct "./createdts.tcl"
 mkdir -p "./${PL_DTS_DIR}"
 cp "${GEN_DTS_PATH}pl.dtsi" "./${PL_DTS_DIR}/"
-cp "${GEN_DTS_PATH}pl-final.dts" "./${PL_DTS_DIR}/"
 cp "${GEN_DTS_PATH}pl-custom.dtsi" "./${PL_DTS_DIR}/"
+sed -e "s/#include/\/include\//" ${GEN_DTS_PATH}pl-final.dts > ./${PL_DTS_DIR}/pl-final.dts
 #cp ../../${PJ_NAME}.runs/impl_1/design_1_wrapper.bin ./pl_dtso/design_1_wrapper.bit.bin
