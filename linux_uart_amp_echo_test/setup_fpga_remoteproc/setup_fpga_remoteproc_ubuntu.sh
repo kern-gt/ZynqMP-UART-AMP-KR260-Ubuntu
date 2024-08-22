f6d463e4cfc6f40908e17ac4abecb6e2be54ad03
sh-goto <sh.goto7000@gmail.com>
2024-08-22 17:01:30 +0900

    I ran shellcheck and fixed it. shellcheckを実行して修正した

--- linux_uart_amp_echo_test/setup_fpga_remoteproc/setup_fpga_remoteproc_ubuntu.sh
+++ linux_uart_amp_echo_test/setup_fpga_remoteproc/setup_fpga_remoteproc_ubuntu.sh
@@ -3,15 +3,15 @@
 # Execution by sudors authority is required.
 # sudors権限による実行が必要です.
 
-cd "$(dirname "$0")"
-WORK_DIR=$(pwd)
+cd "$(dirname "$0")" || exit
+#WORK_DIR=$(pwd)
 SHELL_JSON='{"shell_type" : "XRT_FLAT","num_slots": "1"}'
 FPGA_APP_NAME="uart_amp"
 
 echo "Create FPGA APP /lib/firmware/xilinx/${FPGA_APP_NAME}"
 mkdir -p /lib/firmware/xilinx/${FPGA_APP_NAME}
 
-echo ${SHELL_JSON} > /lib/firmware/xilinx/${FPGA_APP_NAME}/shell.json
+echo "${SHELL_JSON}" > /lib/firmware/xilinx/${FPGA_APP_NAME}/shell.json
 
 cp ./r5_fw/*.elf /lib/firmware/
 cp ./bitstream/*.bin /lib/firmware/xilinx/${FPGA_APP_NAME}/
@@ -19,14 +19,13 @@ cp ./dtbo/*.dtbo /lib/firmware/xilinx/${FPGA_APP_NAME}/
 echo ""
 
 echo "Stop CR5-0 and CR5-1."
-echo stop > /sys/class/remoteproc/remoteproc0/state
-if [ $? -eq 0 ]; then
+if echo stop > /sys/class/remoteproc/remoteproc0/state; then
     echo "  Stop CR5-1 OK"
 else
     echo "  Stop CR5-0 Failed"
 fi
-echo stop > /sys/class/remoteproc/remoteproc1/state
-if [ $? -eq 0 ]; then
+
+if echo stop > /sys/class/remoteproc/remoteproc1/state; then
     echo "  Stop CR5-1 OK"
 else
     echo "  Stop CR5-1 Failed"
@@ -34,15 +33,13 @@ fi
 echo ""
 
 echo "Start FPGA Configuration."
-xmutil unloadapp | grep "(Ok)" > /dev/null
-if [ $? -eq 0 ]; then
+if xmutil unloadapp | grep "(Ok)" > /dev/null; then
     echo "  FPGA unload OK"
 else
     echo "  Err! : FPGA unload Failed"
 fi
 
-xmutil loadapp ${FPGA_APP_NAME} | grep "loaded to slot" > /dev/null
-if [ $? -eq 0 ]; then
+if xmutil loadapp ${FPGA_APP_NAME} | grep "loaded to slot" > /dev/null; then
     echo "  FPGA Configuration OK"
     echo ""
     FPGA_LOAD_SUCCESS="OK"
@@ -54,13 +51,11 @@ fi
 
 if [ ${FPGA_LOAD_SUCCESS} = "OK" ]; then
     echo "Load ELF for CR5-0 and CR5-1."
-    echo app_echo_uart_r5_0.elf > /sys/class/remoteproc/remoteproc0/firmware
 
-    if [ $? -eq 0 ]; then
+    if echo app_echo_uart_r5_0.elf > /sys/class/remoteproc/remoteproc0/firmware; then
         echo "  CR5-0 load OK"
         
-        echo app_echo_uart_r5_1.elf > /sys/class/remoteproc/remoteproc1/firmware
-        if [ $? -eq 0 ]; then
+        if echo app_echo_uart_r5_1.elf > /sys/class/remoteproc/remoteproc1/firmware; then
             echo "  CR5-1 load OK"
             LOAD_ELF_CR5="OK"
         else
@@ -79,11 +74,9 @@ fi
 if [ ${LOAD_ELF_CR5} = "OK" ]; then
     echo ""
     echo "Run CR5-0 and CR5-1."
-    echo start > /sys/class/remoteproc/remoteproc0/state
-    if [ $? -eq 0 ]; then
+    if echo start > /sys/class/remoteproc/remoteproc0/state; then
         echo "  CR5-0 start OK"
-        echo start > /sys/class/remoteproc/remoteproc1/state
-        if [ $? -eq 0 ]; then
+        if echo start > /sys/class/remoteproc/remoteproc1/state; then
             echo "  CR5-1 start OK"
         else
             echo "  Err! : Start CR5-1 Failed"
